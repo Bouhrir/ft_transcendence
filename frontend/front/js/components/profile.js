1f934f49e93385a700fa088eee4c8e9615d6492b
Oussama Bouhrir <obouhrir@c1r4p8.1337.ma>
2024-10-12 21:35:44 +0100

    profile style

--- frontend/front/js/components/profile.js
+++ frontend/front/js/components/profile.js
@@ -1,15 +1,24 @@
 class ProfileComponent extends HTMLElement {
     async connectedCallback(){
         this.innerHTML = `
-        <div>
-            matche
-        </div>
-        <div>
-            <div>profile</div>
-            <div>lastmatchess</div>
-        </div>
-        <div>
-            trophie
+        <div class="profile-acc">
+            <div class="Trophie">
+                <p>HIGHEST PRICE</p>
+                <div class="trophie-type"></div>
+                <p>silver</p>
+            </div>
+            <div class="ProfileAndMatches">
+                <div class="Profile1">
+                    <div class="ProfileImg"></div>
+                    <p>fullName</p>
+                    <p>username</p>
+                    <p>LVL 9</p>
+                </div>
+                <div class="LastMatches">lastmatches</div>
+            </div>
+            <div class="Matche">
+                matches
+            </div>
         </div>
         `;
     }
